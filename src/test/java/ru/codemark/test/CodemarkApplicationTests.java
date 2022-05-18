b094a50bccb9c75129155c2b15db83c7c05825cc
Vladimir Malzam <v.malzam@gmail.com>
2022-05-19 00:58:31 +0300

    Refactoring integration tests 2

--- src/test/java/ru/codemark/test/CodemarkApplicationTests.java
+++ src/test/java/ru/codemark/test/CodemarkApplicationTests.java
@@ -17,7 +17,6 @@ import ru.codemark.test.model.Role;
 import ru.codemark.test.model.User;
 import ru.codemark.test.model.dto.UserDtoForGetAll;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -31,12 +30,12 @@ class CodemarkApplicationTests {
     private static final String URL_ROLE = "/role";
     private static final String URL_USER = "/user";
 
-    private final List<User> emptyUserList = Collections.emptyList();
+    private static final List<User> emptyUserList = Collections.emptyList();
 
-    private final Role targetRoleForTestPut = new Role(1L, "forTestPut", emptyUserList);
-    private final Role targetRoleUser = new Role(2L, "user", emptyUserList);
-    private final Role targetRoleAdmin = new Role(3L, "admin", emptyUserList);
-    private final Role targetRoleAnalyst = new Role(4L, "analyst", emptyUserList);
+    private static final Role targetRoleForTestPut = new Role(1L, "forTestPut", emptyUserList);
+    private static final Role targetRoleUser = new Role(2L, "user", emptyUserList);
+    private static final Role targetRoleAdmin = new Role(3L, "admin", emptyUserList);
+    private static final Role targetRoleAnalyst = new Role(4L, "analyst", emptyUserList);
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -48,28 +47,26 @@ class CodemarkApplicationTests {
     @Order(1)
     void testAddRoles() {
         // Add Role "forTestPut"
-        ResponseEntity<Role> responseEntity = restTemplate.postForEntity(URL_ROLE,
-                new Role(null, "forTestPut", null), Role.class);
+        ResponseEntity<Role> responseEntity = restTemplate
+                .postForEntity(URL_ROLE, new Role(null, "forTestPut", null), Role.class);
 
         assertEquals(new Role(1L, "forTestPut", null), responseEntity.getBody());
         assertEquals(201, responseEntity.getStatusCodeValue());
-
-        // Adding Roles for the next tests
-        restTemplate.postForEntity(URL_ROLE, new Role(null, "user", null), Role.class);
-        restTemplate.postForEntity(URL_ROLE, new Role(null, "admin", null), Role.class);
-        restTemplate.postForEntity(URL_ROLE, new Role(null, "analyst", null), Role.class);
     }
 
     @Test
     @Order(2)
     void testGetAllRoles() {
-        ResponseEntity<Role[]> responseEntity = restTemplate.getForEntity(URL_ROLE, Role[].class);
+        restTemplate.postForEntity(URL_ROLE, new Role(null, "user", null), Role.class);
+        restTemplate.postForEntity(URL_ROLE, new Role(null, "admin", null), Role.class);
+        restTemplate.postForEntity(URL_ROLE, new Role(null, "analyst", null), Role.class);
 
-        List<Role> targetRoleList = new ArrayList<>();
-        targetRoleList.add(targetRoleForTestPut);
-        targetRoleList.add(targetRoleUser);
-        targetRoleList.add(targetRoleAdmin);
-        targetRoleList.add(targetRoleAnalyst);
+        ResponseEntity<Role[]> responseEntity = restTemplate.getForEntity(URL_ROLE, Role[].class);
+        List<Role> targetRoleList = Arrays.asList(
+                targetRoleForTestPut,
+                targetRoleUser,
+                targetRoleAdmin,
+                targetRoleAnalyst);
 
         assertEquals(targetRoleList, Arrays.asList(responseEntity.getBody()));
         assertEquals(200, responseEntity.getStatusCodeValue());
@@ -90,8 +87,8 @@ class CodemarkApplicationTests {
     void testPutRole() {
         // Put Role "forTestPut". Rename to "forTestPut+++"
         Role requestObject = new Role(1L, "forTestPut+++", null);
-        ResponseEntity<Role> responseEntity = restTemplate.exchange(URL_ROLE, HttpMethod.PUT,
-                new HttpEntity<>(requestObject), Role.class);
+        ResponseEntity<Role> responseEntity = restTemplate
+                .exchange(URL_ROLE, HttpMethod.PUT, new HttpEntity<>(requestObject), Role.class);
 
         Role targetRole = requestObject;
 
@@ -120,37 +117,34 @@ class CodemarkApplicationTests {
     @Test
     @Order(6)
     void testAddUsers() {
-        // Add User "John Smith" - Role: user
-        List<Role> roles = new ArrayList<>();
-        roles.add(new Role(2L, null, null));
+        List<Role> roles = Collections.singletonList(new Role(2L, null, null));
         User requestObject = new User("john", "sdH4k", "John Smith", roles);
-        ResponseEntity<User> responseEntity = restTemplate.postForEntity(URL_USER, requestObject,
-                User.class);
+        ResponseEntity<User> responseEntity = restTemplate
+                .postForEntity(URL_USER, requestObject, User.class);
 
-        roles.clear();
-        roles.add(targetRoleUser);
-        User targetUser = new User("john", "sdH4k", "John Smith", roles);
+        List<Role> targetRoles = Collections.singletonList(targetRoleUser);
+        User targetUser = new User("john", "sdH4k", "John Smith", targetRoles);
 
         assertEquals(targetUser, responseEntity.getBody());
         assertEquals(201, responseEntity.getStatusCodeValue());
 
         // Add User for the next tests. "Maria Smith" - Roles: user + admin
-        roles.clear();
-        roles.add(new Role(2L, null, null));
-        roles.add(new Role(3L, null, null));
-        requestObject = new User("maria", "sdF5l", "Maria Smith", roles);
+        List<Role> nextRoles = Arrays.asList(
+                new Role(2L, null, null),
+                new Role(3L, null, null));
+        requestObject = new User("maria", "sdF5l", "Maria Smith", nextRoles);
         restTemplate.postForEntity(URL_USER, requestObject, User.class);
     }
 
     @Test
     @Order(7)
     void testGetAllUsers() {
-        ResponseEntity<UserDtoForGetAll[]> responseEntity = restTemplate.getForEntity(URL_USER,
-                UserDtoForGetAll[].class);
+        ResponseEntity<UserDtoForGetAll[]> responseEntity = restTemplate
+                .getForEntity(URL_USER, UserDtoForGetAll[].class);
 
-        List<UserDtoForGetAll> targetUserList = new ArrayList<>();
-        targetUserList.add(new UserDtoForGetAll("john", "sdH4k", "John Smith"));
-        targetUserList.add(new UserDtoForGetAll("maria", "sdF5l", "Maria Smith"));
+        List<UserDtoForGetAll> targetUserList = Arrays.asList(
+                new UserDtoForGetAll("john", "sdH4k", "John Smith"),
+                new UserDtoForGetAll("maria", "sdF5l", "Maria Smith"));
 
         assertEquals(targetUserList, Arrays.asList(responseEntity.getBody()));
         assertEquals(200, responseEntity.getStatusCodeValue());
@@ -162,9 +156,7 @@ class CodemarkApplicationTests {
         String urlUserAndLogin = URL_USER + "/maria";
         ResponseEntity<User> responseEntity = restTemplate.getForEntity(urlUserAndLogin, User.class);
 
-        List<Role> roles = new ArrayList<>();
-        roles.add(targetRoleUser);
-        roles.add(targetRoleAdmin);
+        List<Role> roles = Arrays.asList(targetRoleUser, targetRoleAdmin);
         User targetUser = new User("maria", "sdF5l", "Maria Smith", roles);
 
         assertEquals(targetUser, responseEntity.getBody());
@@ -175,16 +167,14 @@ class CodemarkApplicationTests {
     @Order(9)
     void testPutUser() {
         // Put User "john". Remove role "user", add roles "admin" and "analyst"
-        List<Role> roles = new ArrayList<>();
-        roles.add(new Role(3L, null, null));
-        roles.add(new Role(4L, null, null));
+        List<Role> roles = Arrays.asList(
+                new Role(3L, null, null),
+                new Role(4L, null, null));
         User requestObject = new User("john", "sdH4k + test", "John Smith + test", roles);
         ResponseEntity<User> responseEntity = restTemplate.exchange(URL_USER, HttpMethod.PUT,
                 new HttpEntity<>(requestObject), User.class);
 
-        List<Role> targetRoles = new ArrayList<>();
-        targetRoles.add(targetRoleAdmin);
-        targetRoles.add(targetRoleAnalyst);
+        List<Role> targetRoles = Arrays.asList(targetRoleAdmin, targetRoleAnalyst);
         User targetUser = new User("john", "sdH4k + test", "John Smith + test", targetRoles);
 
         assertEquals(targetUser, responseEntity.getBody());
